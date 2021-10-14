2cfca8705eaa0aec8e24b872c32379c24954e27f
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-14 09:27:14 -0300

    Añade ultimos cambios

--- App.js
+++ App.js
@@ -1,77 +1,96 @@
-import React from 'react';
-import { 
-  StyleSheet,
-  View,
-  Image,
-  Text,
-  ScrollView
-} from 'react-native';
+import React from "react";
+import { StyleSheet, View, Image, Text, ScrollView } from "react-native";
 
 export default function App() {
   return (
     <>
-    <ScrollView>
-        <View style={{flexDirection: "row"}}>
+      <ScrollView>
+        <View style={{ flexDirection: "row" }}>
           <Image
             style={styles.banner}
-            source={require('./assets/img/bg.jpg')}
+            source={require("./assets/img/bg.jpg")}
           />
         </View>
         <View style={styles.contenedor}>
           <Text style={styles.titulo}>Qué hacer en Paris</Text>
-          <ScrollView
-            horizontal
-          >
+          <ScrollView horizontal>
             <View>
               <Image
-              style={styles.ciudad}
-              source={require('./assets/img/actividad1.jpg')}
+                style={styles.ciudad}
+                source={require("./assets/img/actividad1.jpg")}
               />
             </View>
             <View>
               <Image
-              style={styles.ciudad}
-              source={require('./assets/img/actividad2.jpg')}
+                style={styles.ciudad}
+                source={require("./assets/img/actividad2.jpg")}
               />
             </View>
             <View>
               <Image
-              style={styles.ciudad}
-              source={require('./assets/img/actividad3.jpg')}
+                style={styles.ciudad}
+                source={require("./assets/img/actividad3.jpg")}
               />
             </View>
             <View>
               <Image
-              style={styles.ciudad}
-              source={require('./assets/img/actividad4.jpg')}
+                style={styles.ciudad}
+                source={require("./assets/img/actividad4.jpg")}
               />
             </View>
             <View>
               <Image
-              style={styles.ciudad}
-              source={require('./assets/img/actividad5.jpg')}
+                style={styles.ciudad}
+                source={require("./assets/img/actividad5.jpg")}
               />
             </View>
           </ScrollView>
           <Text style={styles.titulo}>Los mejores alojamientos</Text>
           <View>
             <View>
-                <Image
+              <Image
                 style={styles.mejores}
-                source={require('./assets/img/mejores1.jpg')}
-                />
+                source={require("./assets/img/mejores1.jpg")}
+              />
             </View>
             <View>
-                <Image
+              <Image
                 style={styles.mejores}
-                source={require('./assets/img/mejores2.jpg')}
-                />
+                source={require("./assets/img/mejores2.jpg")}
+              />
             </View>
             <View>
-                <Image
+              <Image
                 style={styles.mejores}
-                source={require('./assets/img/mejores3.jpg')}
-                />
+                source={require("./assets/img/mejores3.jpg")}
+              />
+            </View>
+          </View>
+          <Text style={styles.titulo}>Hospedajes en los Angeles</Text>
+          <View style={styles.listado}>
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require("./assets/img/hospedaje1.jpg")}
+              />
+            </View>
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require("./assets/img/hospedaje2.jpg")}
+              />
+            </View>
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require("./assets/img/hospedaje3.jpg")}
+              />
+            </View>
+            <View style={styles.listadoItem}>
+              <Image
+                style={styles.mejores}
+                source={require("./assets/img/hospedaje4.jpg")}
+              />
             </View>
           </View>
         </View>
@@ -81,29 +100,37 @@ export default function App() {
 }
 //FLEX: se usa como comlumna, toma todo la fila
 // toma toda el espacio del padre
-// si lo asignas a varios view con flex 1, se particiona 
+// si lo asignas a varios view con flex 1, se particiona
 const styles = StyleSheet.create({
-  banner:{
-    height:250,
-    flex:1
+  banner: {
+    height: 250,
+    flex: 1,
   },
-  titulo:{
-    fontWeight: 'bold',
+  titulo: {
+    fontWeight: "bold",
     fontSize: 24,
-    marginVertical: 20
+    marginVertical: 20,
   },
   contenedor: {
-    marginHorizontal:10
+    marginHorizontal: 10,
   },
   ciudad: {
     width: 250,
     height: 300,
-    marginRight: 10
+    marginRight: 10,
   },
-  mejores:{
-    width: '100%',
-    height:200,
+  mejores: {
+    width: "100%",
+    height: 200,
     flex: 1,
-    marginVertical: 5
+    marginVertical: 5,
+  },
+  listado: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  listadoItem:{
+    flexBasis:'49%'
   }
 });
